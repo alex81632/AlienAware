97fce5e39439280cdbc5178ea7d69f423d1a34ff
alex81632 <alexandrebellargus@hotmail.com>
2023-04-03 14:33:47 -0300

    add finish

--- mapTransitions.py
+++ mapTransitions.py
@@ -6,21 +6,34 @@ class mapTransitions:
     def __init__(self, screen, constants):
         self.screen = screen
         self.constants = constants
-        self.font = pg.font.Font('assets/fonts/dogicapixel.ttf', int(self.constants.font_size)*5)  
+        self.font = pg.font.Font('assets/fonts/dogicapixel.ttf', int(self.constants.font_size)*5)
+        self.font_s = pg.font.Font('assets/fonts/dogicapixel.ttf', int(self.constants.font_size))
+        self.time_death = 2
+        self.time_next_map = 1
+        self.time_finish = 4
+        self.time = 10
 
     def draw(self):
-        # se for o mapa 0, significa que houve morte
-        if self.constants.mapa_atual == 0:
-            self.text = self.font.render("Morreu", 1, (200,200,200))
-        else:
-            self.text = self.font.render("Mapa " + str(self.constants.mapa_atual), 1, (200,200,200))
         # fundo preto do tamanho da tela
         pg.draw.rect(self.screen, (0,0,0), (0,0, self.constants.width, self.constants.height))
         # texto no centro da tela
+        if self.constants.outro:
+            self.text = self.font.render("Parabens!", 1, (200,200,200))
+            leg = self.font_s.render("Voce terminou o jogo! Agora está liberado o modo infinito", 1, (200,200,200))
+            self.screen.blit(leg, (self.constants.width/2 - leg.get_width()/2, self.constants.height/2 - leg.get_height()/2 + 100))
+            self.time = self.time_finish
+        # se for o mapa 0, significa que houve morte
+        elif self.constants.mapa_atual == 0:
+            self.text = self.font.render("Morreu", 1, (200,200,200))
+            self.time = self.time_death
+        else:
+            self.text = self.font.render("Mapa " + str(self.constants.mapa_atual), 1, (200,200,200))
+            self.time = self.time_next_map
         self.screen.blit(self.text, (self.constants.width/2 - self.text.get_width()/2, self.constants.height/2 - self.text.get_height()/2))
         # se passou 3 segundos, volta ao jogo
-        if time.time() - self.constants.time > 1:
+        if time.time() - self.constants.time > self.time:
             self.constants.state = 1
+            self.constants.outro = 0
             if self.constants.return_to_menu == True:
                 self.constants.state = 0
                 self.constants.return_to_menu = False
--- waveController.py
+++ waveController.py
@@ -10,15 +10,16 @@ class waveController:
         self.wave_enemies_killed = 0
         self.wave_enemies_alive = 0
         self.wave_enemies_alive_max = 10
-        self.font = pg.font.Font('assets/fonts/dogicapixel.ttf', int(self.game.constants.font_size)//3)
+        self.font = pg.font.Font('assets/fonts/dogicapixel.ttf', int(self.game.constants.font_size)//2)
         self.font_l = pg.font.Font('assets/fonts/dogicapixel.ttf', int(self.game.constants.font_size))
         self.time = time.time()
+        self.timer = 3*6
 
     def update(self):
         while self.wave_enemies_alive < self.wave_enemies_alive_max and self.wave_enemies_spawned < self.wave_enemies:
             self.spawn_enemy()
         # se matou todos os inimigos da wave ou se passaram 3 minutos
-        if self.wave_enemies_killed == self.wave_enemies or time.time() - self.time > 180:
+        if self.wave_enemies_killed == self.wave_enemies or time.time() - self.time > self.timer:
             self.game.constants.finished = 1
             self.game.constants.outro = 1
             self.game.reset_game()
@@ -30,8 +31,8 @@ class waveController:
 
     def draw(self):
         # desenha o tempo que falta para terminar os 3 minutos
-        text = self.font_l.render("Time: " + str(int(180 - (time.time() - self.time))), 1, (200,200,200))
+        text = self.font_l.render("Time: " + str(int(self.timer - (time.time() - self.time))), 1, (200,200,200))
         self.game.screen.blit(text, (self.game.screen.get_width()//2 - text.get_width()//2, 10))
         # desenha a quantidade de inimigos que faltam para terminar a wave
-        text = self.font.render("Enemies: " + str(int(self.wave_enemies - self.wave_enemies_killed)), 1, (200,200,200))
-        self.game.screen.blit(text, (self.game.screen.get_width()//2 - text.get_width()//2, 10 + text.get_height() + 10))
+        text2 = self.font.render("Enemies: " + str(int(self.wave_enemies - self.wave_enemies_killed)), 1, (200,200,200))
+        self.game.screen.blit(text2, (self.game.screen.get_width()//2 - text2.get_width()//2, 10 + text.get_height() + 10))
